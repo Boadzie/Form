8aa52bd92ee80be6bccac4bf5fd17840b5f2e489
Emmanuel <52857931+eeTiboah@users.noreply.github.com>
2020-01-05 23:32:53 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, session, redirect, url_for, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DateTimeField, RadioField, IntegerField, SelectField
+from wtforms.validators import DataRequired, Email
+
+
+
+app=Flask(__name__)
+
+app.config['SECRET_KEY']='secretkeyyy'
+
+class RegistrationForm(FlaskForm):
+	first_name=StringField('First Name', validators=[DataRequired()])
+	last_name=StringField('Last Name', validators=[DataRequired()])
+	birthday=DateTimeField('Birthday',validators=[DataRequired()])
+	gender=RadioField('Gender', choices=[('male', 'Male'), ('female', 'Female')])
+	email=StringField('Email', validators=[DataRequired(), Email()])
+	phone_number=IntegerField('Phone Number', validators=[DataRequired()])
+	subject=SelectField('Choose an option', choices=[('subject1', 'Subject 1'),('subject2', 'Subject 2'), ('subject3', 'Subject 3')])
+	submit=SubmitField('Submit')
+
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+
+	form=RegistrationForm()
+	if form.validate_on_submit():
+		session['firstName'] = form.first_name.data
+		session['lastName'] = form.last_name.data
+		session['birthday'] = form.birthday.data
+		session['gender'] = form.gender.data
+		session['email'] = form.email.data
+		session['phone_number'] = form.phone_number.data
+		session['subject'] = form.subject.data
+
+		return redirect(url_for('show'))
+	return render_template('home.html', form=form)
+
+
+@app.route('/show')
+def show():
+	return render_template('show.html')
+
+
+if __name__=='__main__':
+	app.run(debug=True)
